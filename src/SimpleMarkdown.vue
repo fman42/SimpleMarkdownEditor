52ed607b0ef28935470c29d71aee72a2fab4a99f
fman42 <igrok5554@gmail.com>
2020-11-17 22:52:00 +0300

    Added new functions in toolbar: bold, italic, strikethrough

--- src/SimpleMarkdown.vue
+++ src/SimpleMarkdown.vue
@@ -3,7 +3,7 @@
         <!-- Toolbar -->
         <section class="simple-markdown__toolbar">
             <ul>
-                <li v-for="(item, index) in toolbar.filter(x => !disabledTools.includes(x.id))" v-bind:key="index">
+                <li v-for="(item, index) in toolbar.filter(x => !disabledIdsTools.includes(x.id))" v-bind:key="index">
                     <button v-on:click="executeToolCallback(item.callbackName)">
                         {{ item.name }}
                     </button>
@@ -11,20 +11,26 @@
             </ul>
         </section>
         <!-- Field -->
-        <textarea class="simple-markdown__field" v-on:change="onInputHandler($event)" v-model="content">
+        <textarea class="simple-markdown__field" ref="field" v-on:change="onInputHandler($event)" v-model="content">
             
         </textarea>
-
-        {{ historyStack }}
     </div>
 </template>
 
 <script>
     export default {
         name: 'SimpleMarkdown',
+        props: {
+            disabledTools: {
+                type: Array,
+                default: () => {
+                    return [];
+                }
+            }
+        },
         data() {
             return {
-                disabledTools: [],
+                disabledIdsTools: [],
                 content: ''
             }
         },
@@ -39,6 +45,21 @@
                     id: 'redo',
                     name: 'Redo',
                     callbackName: 'redo'
+                },
+                {
+                    id: 'bold',
+                    name: 'Bold',
+                    callbackName: 'bold'
+                },
+                {
+                    id: 'italic',
+                    name: 'Italic',
+                    callbackName: 'italic'
+                },
+                {
+                    id: 'strike',
+                    name: 'Strikethrough',
+                    callbackName: 'strike'
                 }
             ];
 
@@ -46,6 +67,9 @@
             this.currentIndexHistoryStack = 0;
         },
         mounted() {
+            console.log(this.disabledTools);
+            this.disabledIdsTools = this.disabledTools;
+
             setTimeout(() => {
                 this.pushInHistory(this.content);
             }, 5000);
@@ -53,8 +77,6 @@
         methods: {
             /*
                 Toolbar' callbacks
-                * undo() - return content of field to prev version;
-                * redo() - return content of field to next version;
             */
             undo() {
                 this.content = this.fetchContentFromHistory(true);
@@ -62,6 +84,31 @@
 
             redo() {
                 this.content = this.fetchContentFromHistory(false);
+            },
+
+            bold() {
+                this.supplementFieldContent('**');
+            },
+
+            italic() {
+                this.supplementFieldContent('*');
+            },
+
+            strike() {
+                this.supplementFieldContent('~~');
+            },
+
+            supplementFieldContent(additionalSymbols) {
+                const refFieldStartPosition = this.$refs.field.selectionStart;
+                const refFieldEndPosition = this.$refs.field.selectionEnd;
+
+                if (refFieldStartPosition !== refFieldEndPosition){
+                    const substring = additionalSymbols + `${this.content.slice(refFieldStartPosition, refFieldEndPosition)}` + additionalSymbols;
+                    this.content = this.content.slice(0, refFieldStartPosition) + substring + this.content.slice(refFieldEndPosition, this.content.length);
+                } else 
+                    this.content = additionalSymbols + `${this.content}` + additionalSymbols;
+
+                this.pushInHistory(this.content);
             },
 
             /*
@@ -104,7 +151,25 @@
 </script>
 
 <style scoped>
+    .simple-markdown {
+        width: 45%;
+        background-color: #eff3f7;
+        border-radius: 5px;
+    }
+
+    .simple-markdown__toolbar > ul {
+        margin: 0px;
+        padding: 0px;
+        list-style: none;
+        display: flex;
+    }
+
     .simple-markdown__field {
         resize: none;
+        border: none;
+        outline: none;
+        padding: 5px;
+        width: 100%;
+        height: 100px;
     }
 </style>
